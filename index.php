86af808c28464a67b36da5260f3cc66d7b289869
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-07-05 13:45:15 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+   include_once 'header.php';
+?>
+
+<section class="main-container">
+   <div class="main-wrapper">
+      <h2>Home</h2>
+      <?php
+         if (isset($_SESSION['u_id'])) {
+            echo '<h2>Welome '.$_SESSION['u_first'].' '.$_SESSION['u_last'].'!</h2>';
+         }
+      ?>
+   </div>
+</section>
+
+<?php
+   include_once 'footer.php';
+?>
