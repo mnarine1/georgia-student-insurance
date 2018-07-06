d2aeb8eaedcf2ba72ed482f7d3b5c25fd857020c
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-07-06 18:48:02 -0400

    Add files via upload

--- gsi/makePayment.php
+++ gsi/makePayment.php
@@ -3,6 +3,29 @@
    include_once 'header.php';
 ?>
 
+<section class="main-container">
+   <div class="main-wrapper">
+      <h2>Make a Payment</h2>
+      <form class="signup-form" action="includes/makePayment.inc.php" method="POST" >
+         <input type="hidden" name="value" value="100">
+         <input type="hidden" name="date" value="2001-01-01">
+         <input type="text" name="card" placeholder="Card Number" autocomplete="off">
+         <div class="exp-wrapper">
+            <span>Expiration Date:</span>
+            <input type="text" name="month" placeholder="MM" size="2" maxlength="2">
+            <input type="text" name="year" placeholder="YY" size="2" maxlength="2">
+         </div>
+
+         <input type="text" name="code" placeholder="Security Code">
+         <?php
+            if (isset($_SESSION['u_id'])) {
+               echo '<input type="hidden" name="accID" value="'.$_SESSION['u_acc'].'">';
+            }
+         ?>
+         <input type="submit" name="submit" value="Submit">
+      </form>
+   </div>
+</section>
 
 
 
