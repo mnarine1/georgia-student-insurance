346da680478748e38189752a23e7562f38934d5a
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-07-06 18:48:43 -0400

    Add files via upload

--- gsi/includes/makePayment.inc.php
+++ gsi/includes/makePayment.inc.php
@@ -1,4 +1,36 @@
 <?php
 
-header("Location: ../payments.php/error=notImplemented");
-exit();
+if (isset($_POST['submit'])) {
+   include_once 'dbh.inc.php';
+
+   $value = $_POST['value'];
+   $date = $_POST['date'];
+   $cardNum = $_POST['card'];
+   $expDateMonth = $_POST['month'];
+   $expDateYear = $_POST['year'];
+   $code = $_POST['code'];
+   $prev = 0;
+   $next = 0;
+   $paid = 0;
+   $account = $_POST['accID'];
+
+
+   if (empty($cardNum) || empty($expDateMonth) || empty($expDateYear) || empty($code)) {
+      header("Location: ../makePayment.php?pay=empty");
+      exit();
+   } else {
+      if (!preg_match("/^[0-9]*$/", $cardNum) || !preg_match("/^[0-9]*$/", $expDateMonth) || !preg_match("/^[0-9]*$/", $expDateYear) || !preg_match("/^[0-9]*$/", $code)) {
+         header("Location: ../makePayment.php?pay=notNumber");
+         exit();
+      } else {
+         $paid = 1;
+         $sql = "INSERT INTO payment (Value, DueDate, isPaid, AccountID) VALUES ('$value', '$date', '$paid', '$account');";
+         mysqli_query($conn, $sql);
+         header("Location: ../makePayment?pay=success");
+         exit();
+      }
+   }
+} else {
+   header("Location: ../makePayment.php?pay=error");
+   exit();
+}
